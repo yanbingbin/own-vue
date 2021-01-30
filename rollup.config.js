10c5a57ca4693ace8ffde1471c7bbb48373151dd
颜秉斌 <785175917@qq.com>
2021-01-30 13:29:07 +0800

    refactor: 文件路径修改

--- rollup.config.js
+++ rollup.config.js
@@ -4,7 +4,7 @@ import serve from 'rollup-plugin-serve';
 export default {
     input: './src/index.js', // 打包入口
     output: {
-        file: 'dist/umd/vue.js', // 打包出口
+        file: 'dist/vue.js', // 打包出口
         name: 'Vue', // 指定打包后全局变量的名字
         format: 'umd', // 统一模块规范
         sourcemap: true, // es6 -> es5 开启源码调试，可以找到源代码报错位置
@@ -15,9 +15,9 @@ export default {
         }),
         process.env.ENV === 'development' ? serve({
             open: true,
-            openPage: '/public/index.html', // 默认打开html的路径
-            port: 3000,
+            openPage: '/index.html', // 默认打开html的路径
+            port: 3001,
             contentBase: ''
         }) : null
     ]
-}
+};

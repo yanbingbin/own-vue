77c075ac20c5a9a45682065203b8cfaab5bce868
颜秉斌 <785175917@qq.com>
2021-02-04 22:44:16 +0800

    feat: add mutations actions

--- vuex/store.js
+++ vuex/store.js
@@ -5,6 +5,8 @@ export class Store {
     constructor(options) {
         let state = options.state;
         this.getters = {};
+        this._mutations = {};
+        this._actions = {};
         const computed = {};
         forEachValue(options.getters, (fn, key) => {
             computed[key] = () => { // 将属性放到computed里面
@@ -14,6 +16,12 @@ export class Store {
                 get: () => this._vm[key] // 取的是computed的属性
             });
         });
+        forEachValue(options.mutations, (fn, type) => {
+            this._mutations[type] = (payload) => fn.call(this, this.state, payload);
+        });
+        forEachValue(options.actions, (fn, type) => {
+            this._actions[type] = (payload) => fn.call(this, this.state, payload);
+        });
         this._vm = new Vue({ // 利用 new Vue对state收集依赖，state发生改变时视图更新
             data: { // $$ 代表内部属性，不会被代理到实例上
                 $$state: state
@@ -21,10 +29,17 @@ export class Store {
             computed // 利用计算属性实现缓存
         });
     }
+    commit = (type, payload) => { 
+        this._mutations[type](payload);
+    }
+    dispatch = (type, payload) => {
+        this._actions[type](payload);
+    }
     // 类的属性访问器，当用户去这个实例上取state属性时，会执行这个方法
     get state() {
         return this._vm._data.$$state;
     }
+
 }
 
 let Vue;
